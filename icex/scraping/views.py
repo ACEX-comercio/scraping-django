5e7fb23738a85df2a77d98e774dab7a36b2d357e
tunime <tunime.1@gmail.com>
2018-03-16 17:24:45 -0500

    añadiendo una ruta y enlasando a una vita htlm

--- icex/scraping/views.py
+++ icex/scraping/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+def saludo(request):
+    #req = requests.get(url)
+    #html = BeautifulSoup(req.text, "html.parser")
+    #print html
+    #status_code = req.status_code
+    a=1
+    b=3
+    sum=a+b
+    nombre="Israel"
+    suma=sum
+    blog="https://www.uno-de-piera.com"
+    tupla=(1,2,3,4,5,6,7,8,9,10)
+    context={
+        'saludo':'hola que ase',
+        'tupla':tupla,
+        'nombre':nombre,
+        'blog':blog,
+        'suma':suma
+    }
+    #devolvemos los datos a la vista saludo.html
+    return render(request, 'main.html', context)
