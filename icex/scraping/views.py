c86a9f5318316796545fe234721591486bcbfd64
tunime <tunime.1@gmail.com>
2018-03-19 15:09:49 -0500

    guardado de seguridad funcional

--- icex/scraping/views.py
+++ icex/scraping/views.py
@@ -9,9 +9,26 @@ def saludo(request):
     n_pais = ["Argentina","Bolivia","Brasi","Chile","Colombia","Costa Rica","Cuba","Ecuador","El Salvador","Guayana Francesa","Granada","Guatemala","Guayana","Haiti","Honduras","Jamaica","Mexico","Nicaragua","Paraguay","Panama","Peru","Puerto  Rico","Surinam","Uruguay","Venezuela"]
     ###link del noticiero
     not_url='http://www.bbc.com/mundo/search/?q='
+    ##array de palabras mala
+    #pal_malas=["muertos","virus","deficit","inflacion","destruir","huelgas","manifestaciones","pobreza","terremoto","desolacion","huracan","peligro","mueren","enfrentamiento","instabilidad","ilegales","devaluacion","perjuicio"]
+
+    #DEFNIMOS TODA  LA LOGICA
     for i in range(len(n_pais)):
-        url_pais=not_url+n_pais[i]
-        print url_pais
+        url_pais=not_url+n_pais[i]#unimos el link y las partes
+        #CAPTURAMOS EL CONTNIDO DE LA WEB 
+        req = requests.get(url_pais)
+        html = BeautifulSoup(req.text, "html.parser")
+        #desgregamos y solo capturamos los valores de la clase hard-news-unit__headline-link
+        print n_pais[i]#inprimimos en consola el nombe del pais
+        noticias_pais = html.find_all("a", {"class": "hard-news-unit__headline-link"})#en la variable almasenamos todas la noticias
+        for i, noticia_pais in enumerate(noticias_pais):# en este bucle sacamos todas las noticias
+            noticia=noticia_pais.getText()#cogemos los titulos de las noticias
+            palabra_noticia=noticia.split()#separar el titulo de las noticias
+            for i, palabra in enumerate(palabra_noticia):#e buque que muestra acada palabra de la oracion
+                #crearmos dos arrays uno de buenas palabras y la otra de palabras malas
+                #y recoremos con un for y dentro un caomparador de palabras y al termina le damos un puntuacion
+                print (palabra)
+            print (palabra_noticia)
     a=1
     b=3
     sum=a+b
